f2d5f789cd54df9eaecb66f88d356efdb419992a
shenyineng <ieiayaobb@qq.com>
2017-01-10 13:46:02 +0800

    remove unit tests

---
+++ test/unit/specs/FrameworkView.spec.js
@@ -0,0 +1,13 @@
+// import Vue from 'vue'
+// import Framework from 'src/components/Framework'
+
+describe('Framework.vue', () => {
+  // it('should render correct contents', () => {
+  //   const vm = new Vue({
+  //     el: document.createElement('div'),
+  //     render: h => h(Framework)
+  //   }).$mount()
+
+  //   expect(vm.$el.querySelector('h1').textContent).to.contain('Welcome.')
+  // })
+})
